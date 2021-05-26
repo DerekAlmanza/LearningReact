6a3fa35235b3f5da5b28898763b8dcd5ebbf74b0
DerekAlmanza <Derek.Almanza@ciencias.unam.mx>
2021-05-26 16:27:01 -0500

    Desestructuracion de objetos y arreglos, import y export de la app 02 de React añadido.

---
+++ 02-repaso-js/src/bases/desesctructurarArreglos.js
@@ -0,0 +1,27 @@
+/* Desestructuración de arreglos. */
+
+const personajes = ['Harry', 'Ron', 'Hermione'];
+
+const [ , ron, hermione] = personajes;
+
+console.log(ron, hermione);
+
+// Desestructurar por medio de función.
+
+const getArreglo = () => {
+    return ['ABC', 123];
+}
+
+const [letras, numeros] = getArreglo();
+console.log(letras, numeros);
+
+// Ejercicio 
+
+const useState = (valor) => {
+    return [valor, () => { console.log('Tu nombre es'); }]
+}
+
+const [nombre, setNombre] = useState('Lalo');
+
+setNombre();
+console.log(nombre);
---
+++ 02-repaso-js/src/bases/importExport.js
@@ -0,0 +1,22 @@
+/* Import, export y funciones comunes de arreglos. */
+import heroes, { propietarios } from '../data/heroes';
+// Esta funciona con el exports default import heroes from './data/heroes'
+
+console.log(heroes);
+console.log(propietarios);
+
+// Ejercicio. Obtener heroe por medio del id.
+
+const getHeroById = (id) => {
+    return heroes.find(heroe => heroe.id === id);
+}
+
+console.log(getHeroById(2));
+
+// Obtener heroes por medio del propietario del héroe.
+
+const getHeroByOwner = (owner) => {
+    return heroes.filter(heroe => heroe.owner === owner);
+}
+
+console.log(getHeroByOwner('DC'));
